94ab3e8f7e95bc660f23816407a8ad7d7f03941f
KID1412-ux <xiangtanliuyu@126.com>
2021-04-27 08:05:35 +0800

    Commit

--- code-web/src/main/java/com/guigu/code/controller/CarController.java
+++ code-web/src/main/java/com/guigu/code/controller/CarController.java
@@ -3,6 +3,7 @@ package com.guigu.code.controller;
 import com.guigu.code.pojo.Car;
 import com.guigu.code.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("car")
+@CrossOrigin
 public class CarController {
     @Autowired
     private CarService carService;
