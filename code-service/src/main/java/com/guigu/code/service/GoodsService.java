b6ef6944270bb99ca862e41b85ca321d99edf7df
ZJ <1347270593@qq.com>
2021-05-06 09:48:08 +0800

    商品信息维护

---
+++ code-service/src/main/java/com/guigu/code/service/GoodsService.java
@@ -0,0 +1,7 @@
+package com.guigu.code.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.guigu.code.pojo.Goods;
+
+public interface GoodsService extends IService<Goods> {
+}
